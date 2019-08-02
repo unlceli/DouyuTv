bb8e22cdaa2fe2783c97c7ab1c88f0dbd58f9074
lijj <125956902@qq.com>
2019-08-02 09:45:00 +0800

    初始化程序

---
+++ DouYTV/DouYTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DouYTV
+//
+//  Created by jdknet on 2019/8/2.
+//  Copyright © 2019年 jdknet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
